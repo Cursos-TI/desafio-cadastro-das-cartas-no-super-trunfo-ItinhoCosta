730abfe06aec25aa132b284ea085b8fe2a435624
ItinhoCosta <digitaisemprendimentos@gmail.com>
2025-08-11 10:04:01 -0300

    Atualizar o CartasSuperTrunfo.c
    Adicionado as variáveis Estado e Cidade

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -3,18 +3,24 @@
 int main() {
     printf ("Desafio Super Trunfo!\n");
     // Declaração das variáveis para a Carta 1
-    char codigo1[4];
+    char codigo1[10], estado1[20], cidade1[50];
     int populacao1;
     float area1, pib1;
     int pontos_turisticos1;
 
     // Declaração das variáveis para a Carta 2
-    char codigo2[4];
+    char codigo2[10], estado2[20], cidade2[50];
     int populacao2;
     float area2, pib2;
     int pontos_turisticos2;
 
      // Cadastro da Carta 1
+    printf("Digite o estado da Carta 1 (ex: Alagoas): ");
+    scanf("%s", &estado1);
+
+    printf("Digite a cidade da Carta 1 (ex: Maceio): ");
+    scanf("%s", &cidade1);
+
     printf("Digite o codigo da Carta 1 (ex: A01): ");
     scanf("%s", codigo1);
 
@@ -31,6 +37,13 @@ int main() {
     scanf("%d", &pontos_turisticos1);
 
      // Cadastro da Carta 2
+
+    printf("Digite o estado da Carta 2 (ex: Alagoas): ");
+    scanf("%s", &estado2);
+
+    printf("Digite a cidade da Carta 2 (ex: Maceio): ");
+    scanf("%s", &cidade2);
+
     printf("\nDigite o codigo da Carta 2 (ex: B02): ");
     scanf("%s", codigo2);
 
@@ -48,6 +61,8 @@ int main() {
 
      // Exibição dos dados cadastrados
     printf("\n--- Dados da Carta 1 ---\n");
+    printf("Estado: %s\n", estado1);
+    printf("Cidade: %s\n", cidade1);
     printf("Codigo: %s\n", codigo1);
     printf("Populacao: %d\n", populacao1);
     printf("Area: %.2f km²\n", area1);
@@ -55,6 +70,8 @@ int main() {
     printf("Pontos Turisticos: %d\n", pontos_turisticos1);
 
     printf("\n--- Dados da Carta 2 ---\n");
+    printf("Estado: %s\n", estado2);
+    printf("Cidade: %s\n", cidade2);
     printf("Codigo: %s\n", codigo2);
     printf("Populacao: %d\n", populacao2);
     printf("Area: %.2f km²\n", area2);
